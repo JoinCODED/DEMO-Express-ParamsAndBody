654c66c6538a7952b1779990f60d0dd3264dc6b8
Lailz <lailaabdulraheem@gmail.com>
2021-10-10 23:14:19 +0300

    add endpoint

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const cookies = require("./cookies");
 
 const app = express();
 
+app.use(express.json());
+
 // Cookie List
 app.get("/cookies", (req, res) => {
   res.json(cookies);
+});
+
+app.get("/cookies/:cookieId", (req, res) => {
+  const cookie = cookies.find((cookie) => cookie.id === +req.params.cookieId);
+  res.json(cookie);
+});
+
+app.post("/cookies", (req, res) => {
+  console.log(req.body);
 });
 
 const PORT = 8000;
